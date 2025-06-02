c0a374daf950774c78a8204996925ce5e438de24
Scott Jones <scottjoneswilliam@gmail.com>
2025-06-02 18:58:58 +0100

    first implementation of dino

--- dreamerv3/rssm.py
+++ dreamerv3/rssm.py
@@ -12,6 +12,7 @@ import numpy as np
 
 import torch
 import perception_models.core.vision_encoder.pe as pe
+from transformers import AutoImageProcessor, FlaxDinov2Model
 
 from torch.nn import functional as F
 from PIL import Image
@@ -421,290 +422,45 @@ class PEEncoder(Encoder):
     x = x.reshape((x.shape[0], -1))
     return x
 
-"""
-(dreamer) [swj24@gpu-q-5 dreamerv3]$ python dreamerv3/main.py --logdir ./logdir/pe_test --configs atari100kPE
----  ___                           __   ______ ---
---- |   \ _ _ ___ __ _ _ __  ___ _ \ \ / /__ / ---
---- | |) | '_/ -_) _` | '  \/ -_) '/\ V / |_ \ ---
---- |___/|_| \___\__,_|_|_|_\___|_|  \_/ |___/ ---
-Replica: 0 / 1
-Logdir: logdir/pe_test
-Run script: train
-A.L.E: Arcade Learning Environment (version 0.9.0+750d7f9)
-[Powered by Stella]
-Missing keys for loading vision encoder: []
-Unexpected keys for loading vision encoder: []
-Observations
-  image            Space(uint8, shape=(224, 224, 3), low=0, high=255)
-  reward           Space(float32, shape=(), low=-inf, high=inf)
-  is_first         Space(bool, shape=(), low=False, high=True)
-  is_last          Space(bool, shape=(), low=False, high=True)
-  is_terminal      Space(bool, shape=(), low=False, high=True)
-Actions
-  action           Space(int32, shape=(), low=0, high=6)
-Extras
-  consec           Space(int32, shape=(), low=-2147483648, high=2147483647)
-  stepid           Space(uint8, shape=(20,), low=0, high=255)
-  dyn/deter        Space(float32, shape=(8192,), low=-inf, high=inf)
-  dyn/stoch        Space(float32, shape=(32, 64), low=-inf, high=inf)
-JAX devices (1): [cuda:0]
-Policy devices: cuda:0
-Train devices:  cuda:0
-Initializing parameters...
-image
-image
-Optimizer opt has 300,954,888 params:
-   161,932,035 dec
-    92,338,176 dyn
-    12,850,431 val
-    12,595,206 pol
-    10,749,183 rew
-    10,488,833 con
-         1,024 enc
-Done initializing!
-Compiling 1 checkpoint groups...
-Largest checkpoint group: 3 GB
-Compiling train and report...
-image
-image
-Train cost analysis:
-  FLOPS:            5.4e+11
-  Memory (temp):    3.3e+10
-  Memory (inputs):  3.9e+09
-  Memory (outputs): 3.7e+09
-  Memory (code):    3.5e+06
-
-image
-Report cost analysis:
-  FLOPS:            8.2e+09
-  Memory (temp):    1.8e+09
-  Memory (inputs):  1.1e+09
-  Memory (outputs): 1.2e+08
-  Memory (code):    4.2e+05
-
-Done compiling!
-Did not find any checkpoint.
-Saving checkpoint: logdir/pe_test/ckpt/20250529T113411F867307
-Saved checkpoint.
-Start training loop
-E0529 11:34:21.985197 3294497 pjrt_stream_executor_client.cc:3067] Execution of replica 0 failed: INTERNAL: CustomCall failed: CpuCallback error: Traceback (most recent call last):
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/dreamerv3/main.py", line 277, in <module>
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/dreamerv3/main.py", line 69, in main
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/embodied/run/train.py", line 97, in train
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/embodied/core/driver.py", line 54, in __call__
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/embodied/core/driver.py", line 70, in _step
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/embodied/run/train.py", line 93, in <lambda>
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/contextlib.py", line 81, in inner
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/embodied/jax/agent.py", line 238, in policy
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/traceback_util.py", line 180, in reraise_with_filtered_traceback
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/pjit.py", line 332, in cache_miss
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/pjit.py", line 190, in _python_pjit_helper
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/core.py", line 2782, in bind
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/core.py", line 443, in bind_with_trace
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/core.py", line 949, in process_primitive
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/pjit.py", line 1739, in _pjit_call_impl
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/pjit.py", line 1721, in call_impl_cache_miss
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/pjit.py", line 1675, in _pjit_call_impl_python
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/profiler.py", line 333, in wrapper
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/interpreters/pxla.py", line 1277, in __call__
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/interpreters/mlir.py", line 2777, in _wrapped_callback
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/callback.py", line 228, in _callback
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/callback.py", line 78, in pure_callback_impl
-RuntimeError: jax.pure_callback failed to find a local CPU device to place the inputs on. Make sure "cpu" is listed in --jax_platforms or the JAX_PLATFORMS environment variable.
-Traceback (most recent call last):
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/dreamerv3/main.py", line 277, in <module>
-    main()
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/dreamerv3/main.py", line 69, in main
-    embodied.run.train(
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/embodied/run/train.py", line 97, in train
-    driver(policy, steps=10)
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/embodied/core/driver.py", line 54, in __call__
-    step, episode = self._step(policy, step, episode)
-                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/embodied/core/driver.py", line 70, in _step
-    self.carry, acts, outs = policy(self.carry, obs, **self.kwargs)
-                             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/embodied/run/train.py", line 93, in <lambda>
-    policy = lambda *args: agent.policy(*args, mode='train')
-                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/contextlib.py", line 81, in inner
-    return func(*args, **kwds)
-           ^^^^^^^^^^^^^^^^^^^
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/embodied/jax/agent.py", line 238, in policy
-    carry, acts, outs = self._policy(
-                        ^^^^^^^^^^^^^
-jaxlib.xla_extension.XlaRuntimeError: INTERNAL: CustomCall failed: CpuCallback error: Traceback (most recent call last):
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/dreamerv3/main.py", line 277, in <module>
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/dreamerv3/main.py", line 69, in main
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/embodied/run/train.py", line 97, in train
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/embodied/core/driver.py", line 54, in __call__
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/embodied/core/driver.py", line 70, in _step
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/embodied/run/train.py", line 93, in <lambda>
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/contextlib.py", line 81, in inner
-  File "/rds/user/swj24/hpc-work/dissertation/dreamerv3/embodied/jax/agent.py", line 238, in policy
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/traceback_util.py", line 180, in reraise_with_filtered_traceback
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/pjit.py", line 332, in cache_miss
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/pjit.py", line 190, in _python_pjit_helper
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/core.py", line 2782, in bind
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/core.py", line 443, in bind_with_trace
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/core.py", line 949, in process_primitive
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/pjit.py", line 1739, in _pjit_call_impl
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/pjit.py", line 1721, in call_impl_cache_miss
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/pjit.py", line 1675, in _pjit_call_impl_python
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/profiler.py", line 333, in wrapper
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/interpreters/pxla.py", line 1277, in __call__
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/interpreters/mlir.py", line 2777, in _wrapped_callback
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/callback.py", line 228, in _callback
-  File "/home/swj24/.conda/envs/dreamer/lib/python3.11/site-packages/jax/_src/callback.py", line 78, in pure_callback_impl
-RuntimeError: jax.pure_callback failed to find a local CPU device to place the inputs on. Make sure "cpu" is listed in --jax_platforms or the JAX_PLATFORMS environment variable.
---------------------
-For simplicity, JAX has removed its internal frames from the traceback of the following exception. Set JAX_TRACEBACK_FILTERING=off to include these.
-"""
-class Decoder(nj.Module):
-
+class DINOv2Encoder(Encoder):
+  """
+  Use facebook perception encoder to encode image observation producing a single 1024 vector.
+  Compared to default vector of length 4x4x256=4096.
+  """
   units: int = 1024
   norm: str = 'rms'
   act: str = 'gelu'
-  outscale: float = 1.0
   depth: int = 64
   mults: tuple = (2, 3, 4, 4)
   layers: int = 3
   kernel: int = 5
   symlog: bool = True
-  bspace: int = 8
   outer: bool = False
   strided: bool = False
 
   def __init__(self, obs_space, **kw):
-    assert all(len(s.shape) <= 3 for s in obs_space.values()), obs_space
-    self.obs_space = obs_space
+    super().__init__(obs_space, **kw)
 
-    # Separate out vector and image observations
-    self.veckeys = [k for k, s in obs_space.items() if len(s.shape) <= 2]
-    self.imgkeys = [k for k, s in obs_space.items() if len(s.shape) == 3]
-
-    # The output channels of the CNN layers
-    self.depths = tuple(self.depth * mult for mult in self.mults)
-
-    # Get the channels and resolutions in the image observations
-    self.imgdep = sum(obs_space[k].shape[-1] for k in self.imgkeys)
-    self.imgres = self.imgkeys and obs_space[self.imgkeys[0]].shape[:-1]
-    self.kw = kw
-
-  @property
-  def entry_space(self):
-    return {}
-
-  def initial(self, batch_size):
-    return {}
-
-  def truncate(self, entries, carry=None):
-    return {}
-
-  def __call__(self, carry, feat, reset, training, single=False):
-    assert feat['deter'].shape[-1] % self.bspace == 0
+    model = FlaxDinov2Model.from_pretrained("facebook/dinov2-small", from_pt=True)
+    self.dino = self.sub('dino_enc', nj.FromFlax, model)
+  
+  def encode_image_obs(self, obs, bdims):
     K = self.kernel
-    recons = {}
-    bshape = reset.shape
+    imgs = [obs[k] for k in sorted(self.imgkeys)]
+    assert all(x.dtype == jnp.uint8 for x in imgs)
 
-    # Input is the array of the latent state and recurrent state
-    inp = [nn.cast(feat[k]) for k in ('stoch', 'deter')]
+    x = nn.cast(jnp.concatenate(imgs, -1), force=True) / 255 - 0.5
+    x = x.reshape((-1, *x.shape[bdims:]))
 
-    # Flatten the batch and time dimensions and concatenate
-    inp = [x.reshape((math.prod(bshape), -1)) for x in inp]
-    inp = jnp.concatenate(inp, -1)
+    # Forward pass through the image encoder
+    x = self.dino(x).last_hidden_state
 
-    if self.veckeys:
-      # Create a mapping for vector observations to output types
-      spaces = {k: self.obs_space[k] for k in self.veckeys}
-      o1, o2 = 'categorical', ('symlog_mse' if self.symlog else 'mse')
-      outputs = {k: o1 if v.discrete else o2 for k, v in spaces.items()}
+    # Apply activation and normalization
+    x = nn.act(self.act)(self.sub(f'cnn{i}norm', nn.Norm, self.norm)(x))
 
-      # Forward pass of the MLP for vector observations and recover batch and time dimensions
-      kw = dict(**self.kw, act=self.act, norm=self.norm)
-      x = self.sub('mlp', nn.MLP, self.layers, self.units, **kw)(inp)
-      x = x.reshape((*bshape, *x.shape[1:]))
+    assert 3 <= x.shape[-3] <= 16, x.shape
+    assert 3 <= x.shape[-2] <= 16, x.shape
+    # Flatten all dimensions except the first (batch dimension)
+    x = x.reshape((x.shape[0], -1))
 
-      # Output distributions over the vector observations
-      kw = dict(**self.kw, outscale=self.outscale)
-      outs = self.sub('vec', embodied.jax.DictHead, spaces, outputs, **kw)(x)
-      recons.update(outs)
-
-    if self.imgkeys:
-      # Determine the scaling from original image to encoded resolution
-      # other than the outer layer we halve the resolution at each layer
-      factor = 2 ** (len(self.depths) - int(bool(self.outer)))
-      minres = [int(x // factor) for x in self.imgres]
-
-      assert 3 <= minres[0] <= 16, minres
-      assert 3 <= minres[1] <= 16, minres
-      shape = (*minres, self.depths[-1])
-
-      # If we have a bspace, we separate the deterministic and stochastic features
-      if self.bspace:
-        u, g = math.prod(shape), self.bspace
-        x0, x1 = nn.cast((feat['deter'], feat['stoch']))
-        x1 = x1.reshape((*x1.shape[:-2], -1))
-        x0 = x0.reshape((-1, x0.shape[-1]))
-        x1 = x1.reshape((-1, x1.shape[-1]))
-        
-        # forward pass on deterministic features 
-        x0 = self.sub('sp0', nn.BlockLinear, u, g, **self.kw)(x0)
-        x0 = einops.rearrange(
-            x0, '... (g h w c) -> ... h w (g c)',
-            h=minres[0], w=minres[1], g=g)
-
-      # forward pass on stochastic features
-        x1 = self.sub('sp1', nn.Linear, 2 * self.units, **self.kw)(x1)
-        x1 = nn.act(self.act)(self.sub('sp1norm', nn.Norm, self.norm)(x1))
-        x1 = self.sub('sp2', nn.Linear, shape, **self.kw)(x1)
-
-        # Final activation and normalisation on the sum of the two parts
-        x = nn.act(self.act)(self.sub('spnorm', nn.Norm, self.norm)(x0 + x1))
-
-      else:
-        # if not bspace, we just pass the input through a linear layer
-        x = self.sub('space', nn.Linear, shape, **kw)(inp)
-        x = nn.act(self.act)(self.sub('spacenorm', nn.Norm, self.norm)(x))
-
-      for i, depth in reversed(list(enumerate(self.depths[:-1]))):
-        # use a strided transposed convolution to upsample the image
-        if self.strided:
-          kw = dict(**self.kw, transp=True)
-          x = self.sub(f'conv{i}', nn.Conv2D, depth, K, 2, **kw)(x)
-
-        # use standard convolution along with upsampling/cloning values along the width and height
-        else:
-          x = x.repeat(2, -2).repeat(2, -3)
-          x = self.sub(f'conv{i}', nn.Conv2D, depth, K, **self.kw)(x)
-
-        x = nn.act(self.act)(self.sub(f'conv{i}norm', nn.Norm, self.norm)(x))
-
-      # Last layer produces the correct number of output channels
-      if self.outer:
-        kw = dict(**self.kw, outscale=self.outscale)
-        x = self.sub('imgout', nn.Conv2D, self.imgdep, K, **kw)(x)
-
-      elif self.strided:
-        kw = dict(**self.kw, outscale=self.outscale, transp=True)
-        x = self.sub('imgout', nn.Conv2D, self.imgdep, K, 2, **kw)(x)
-        
-      else:
-        x = x.repeat(2, -2).repeat(2, -3)
-        kw = dict(**self.kw, outscale=self.outscale)
-        x = self.sub('imgout', nn.Conv2D, self.imgdep, K, **kw)(x)
-
-      # Shrink values between 0 and 1
-      x = jax.nn.sigmoid(x)
-      x = x.reshape((*bshape, *x.shape[1:]))
-    
-      # Split the output tensor into separate images for each key
-      split = np.cumsum(
-          [self.obs_space[k].shape[-1] for k in self.imgkeys][:-1])
-      for k, out in zip(self.imgkeys, jnp.split(x, split, -1)):
-        out = embodied.jax.outs.MSE(out)
-        out = embodied.jax.outs.Agg(out, 3, jnp.sum)
-        recons[k] = out
-
-    entries = {}
-    return carry, entries, recons
+    return x
