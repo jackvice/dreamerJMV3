d7b6ce8ccc26c9f15ef5f0f8b89c364b2f61cebc
Scott Jones <scottjoneswilliam@gmail.com>
2025-06-06 18:04:55 +0100

    fix rgb image retrieval

--- embodied/envs/maniskill.py
+++ embodied/envs/maniskill.py
@@ -1,8 +1,9 @@
-
+import elements
 import embodied
 import mani_skill.envs
 from mani_skill.utils.wrappers.flatten import FlattenRGBDObservationWrapper, FlattenActionSpaceWrapper
 import gymnasium as gym
+import numpy as np
 
 class ManiSkill(embodied.Env):
   def __init__(self, task, size=(64, 64), resize='pillow', **kwargs):
@@ -23,7 +24,7 @@ class ManiSkill(embodied.Env):
   @property
   def obs_space(self):
     spaces = self.env.obs_space.copy()
-    spaces['image'] = spaces['rgb']
+    spaces['image'] = elements.Space(np.uint8, (*self.size, 3))
     del spaces['rgb']
     return spaces
 
@@ -33,6 +34,19 @@ class ManiSkill(embodied.Env):
 
   def step(self, action):
     obs = self.env.step(action)
-    obs['image'] = obs['rgb']
-    del obs['rgb']
     return obs
+  
+  def _obs(self, obs, reward, is_first=False, is_last=False, is_terminal=False):
+    if not (self._obs_dict and isinstance(obs, dict)):
+      obs = {self._obs_key: obs['rgb']}
+
+    del obs['rgb']
+    obs = self._flatten(obs)
+    obs = {k: np.asarray(v) for k, v in obs.items()}
+    obs.update(
+        reward=np.float32(reward),
+        is_first=is_first,
+        is_last=is_last,
+        is_terminal=is_terminal)
+    return obs
+
